3b9eac4143b16729db267253cff1baa417a60423
Aathhh <135854582+Aathhh@users.noreply.github.com>
2025-01-22 11:20:32 +0100

    pushed

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import car_class 
+import road_class
+
+
--- road_class.py
+++ road_class.py
@@ -1,22 +1,18 @@
 import numpy as np 
-#import car_class as car 
 
 class Road():   #This class as car as an input parameter
     def __init__(self, length = 100):
-        self.length = length
+        self.length = np.array([0,length])
 
-    def dynamics(self, car):
+    def dynamics(self, car, dt = 1):
 
         car.speed = car.initial_speed   # initial speed of the car
+        car.position = car.initial_position
         distance = 0 # at the beginning of the road
 
-        for dt in range(self.length):
+        while car.position(1) < self.length(1):
 
-            distance += car.speed * dt
+            car.position += car.speed * dt
             print(f'{distance} m travelled after a time {dt} s at speed {car.speed} m/s')
-
-            if distance > self.length:
-                print('End of road')
-                break
 
         return car.speed
