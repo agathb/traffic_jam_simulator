42200f7cf8021ea1952bfe4f8697f4e5a2cc2ec1
FCalavaro <filippo.calavaro@gmail.com>
2025-01-22 11:40:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import numpy as np
-import car_class
-import road_class
+import sys
 
-car1 = Car(np.arange(1, 3, 1), 2)
+sys.path.append('/Users/fcalavaro/Documents/GitHub/traffic_jam_simulator/')
+
+import numpy as np
+import car_class as c
+import road_class as r
+
+car1 = c.Car(np.arange(1, 3, 1), 2)
 
 print(car1.initial_position)
