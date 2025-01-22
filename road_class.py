ed7d0797e888b5b1dfe57b2e14884fbf5a39386b
Aathhh <135854582+Aathhh@users.noreply.github.com>
2025-01-22 11:25:29 +0100

    push

--- road_class.py
+++ road_class.py
@@ -4,15 +4,14 @@ class Road():   #This class as car as an input parameter
     def __init__(self, length = 100):
         self.length = np.array([0,length])
 
-    def dynamics(self, car, dt = 1):
+    def dynamics(self, car, dt = np.array([1,1])):
 
         car.speed = car.initial_speed   # initial speed of the car
-        car.position = car.initial_position
-        distance = 0 # at the beginning of the road
+        car.position = car.initial_position   # initial position of the car
 
         while car.position(1) < self.length(1):
 
             car.position += car.speed * dt
-            print(f'{distance} m travelled after a time {dt} s at speed {car.speed} m/s')
+            print(f'car at {car.position} after a time {dt} s at speed {car.speed} m/s')
 
         return car.speed
