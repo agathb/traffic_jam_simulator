c730738add00c43d9d405bfd99924b4f584909c8
FCalavaro <filippo.calavaro@gmail.com>
2025-01-29 10:46:45 +0100

    update

--- car_class.py
+++ car_class.py
@@ -4,12 +4,26 @@ class Car:
     
     def __init__(self,
                  position,
-                 speed):
+                 speed,
+                 time_step):
         
         self.position = position
         self.speed = speed
+        self.time_step = time_step
 
+    def move(self, time_step):
 
+        position_history = []
+        time_step = np.array([time_step, time_step])
+        current_time = 0
+
+        while movement = True: # car.position[1] is the position of the front of the car
+            position_memory.append(car.position[1])
+            car.position += car.speed * time_step
+            time += dt
+            print(f'car at {car.position} after a time {time} s at speed {car.speed} m/s')
+
+        return time, dt, position_memory
 
 # Test
         
