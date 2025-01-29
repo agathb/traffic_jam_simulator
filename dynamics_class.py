b9bf1fe1bd82d5a0dcd32bd4cb7594ec9d572f04
Aathhh <135854582+Aathhh@users.noreply.github.com>
2025-01-29 12:34:04 +0100

    neeed to fix the animation

--- dynamics_class.py
+++ dynamics_class.py
@@ -19,10 +19,10 @@ class Dynamics():
 
         while car.ability_to_move == True:
 
-            position_history.append(car.move(self.time_step))
+            position_history.append(car.move_straight(self.time_step))
             time += self.time_step
 
-            if car.position[1] >= road.length:
+            if car.position[1] >= road.length[1]:
 
                 car.ability_to_move = False
                 print(f'\nCar stopped at the end of the road after a time of {time} s.')
@@ -34,8 +34,9 @@ class Dynamics():
         fig, ax =  plt.subplots()
 
         time, position = self.dynamics(car, road)
+        print(position)
         x_axis = np.linspace(0, road.length, len(position))
-        car_position, = ax.errorbar(x_axis, position, self.map, yerr = self.car_size(car)/2) 
+        car_position, = ax.errorbar(x_axis, position, yerr = car.length/2, xerr=car.width/2) 
 
         def animate(i):
             car_position.set_ydata(position[i])  # update the data.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import car_class as c
 import road_class as r
+import dynamics_class as d
 
 time_step = 1
 
@@ -8,5 +9,8 @@ car1 = c.Car(np.array([1, 6]), 10)
 
 print(car1.position)
 
-road = r.Road()
-road.plot(car1)
+road = r.Road(100)
+#road.plot(car1)
+
+dynamics = d.Dynamics(time_step)
+dynamics.animated_plot(car1, road)
