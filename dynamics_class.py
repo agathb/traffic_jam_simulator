ca010cddf5691734d0066fbfc873e1693cc0fff0
FCalavaro <filippo.calavaro@gmail.com>
2025-01-29 12:04:45 +0100

    update

--- dynamics_class.py
+++ dynamics_class.py
@@ -3,21 +3,29 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 class Dynamics():
-    def __init__(self, time_step):
+
+    def __init__(self,
+                 time_step):
+
         self.time_step = time_step
 
-    def dynamics(self, car, road):
+    def dynamics(self,
+                 car,
+                 road):
+
         car.ability_to_move = True
         position_history = [car.position]
         time = 0
 
         while car.ability_to_move == True:
+
             position_history.append(car.move(self.time_step))
             time += self.time_step
 
-            if car.position[1] > road.length:
+            if car.position[1] >= road.length:
+
                 car.ability_to_move = False
-                print(f'Car stopped at the end of the road after a time {time} s.')
+                print(f'\nCar stopped at the end of the road after a time of {time} s.')
 
         return time, position_history
 
