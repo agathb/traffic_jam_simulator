6b60019f3761f09580f9c89209967aa12e47c14e
Aathhh <135854582+Aathhh@users.noreply.github.com>
2025-03-07 11:32:37 +0100

    example semi working

---
+++ dynamics_class_example.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+class Dynamics:
+
+    def __init__(self,
+                 time_step):
+
+        self.time_step = time_step
+
+    def check_intial_car_positions(self, 
+                                   cars, 
+                                   road):
+
+        # For each car in cars list
+        for index, car in enumerate(cars):
+
+            # If the front or back of each car is not on the road initially, stop the program
+            if car.position[1] >= road.end_position or car.position[0] <= road.starting_position:
+                raise ValueError(f'\nCar {index} is not on the road.')
+            
+           # For all the cars except the last car
+            if index in range(len(cars)-1):
+
+                # If the back of the car is overlapping with the front of the car behind, stop the program
+                if car.position[0] < cars[index+1].position[1]: 
+                    raise ValueError(f'\nCar {index} is overlapping with car {index+1}.')
+
+    def no_impact(self, 
+                  car, 
+                  car_in_front):
+
+        # If the front of the car is within 1m of the back of the car in front, return false
+        if car.position[1] >= car_in_front.position[0] - 1:
+            return False
+        else :
+            return True
+
+
+    def dynamics(self,
+                 cars, 
+                 road):
+        
+        # Check the initial car position, will return an error if the cars are not on the road or overlapping
+        self.check_intial_car_positions(cars, road)
+
+        # Initialize the position history with the initial positions of the cars and set the time to 0
+        position_history = [cars.get_positions()]
+        time = 0
+
+        # While at least one car is still on the road, keep moving the cars.
+        while np.any(cars.get_positions() < road.end_position) : 
+            initial_speed = cars.get_speeds()
+
+            for index, car in enumerate(cars):
+
+                # Create a copy of the car position before moving and a copy of the car position after moving
+                before_moving = car.position.copy()
+                after_moving = car.move(self.time_step)
+
+                # If the car is the first car, it can always move because there is no car in front 
+                if index == 0:
+                    car.ability_to_move = True            
+                # Else, check if moving does not make it impact the car in front (no_impact returns false if there is an impact)              
+                else :
+                    car.ability_to_move = self.no_impact(car, cars[index-1])
+                    if index == 1:
+                        if time == 1:
+                            car.speed = car.speed/3
+                        else :
+                            car.speed = initial_speed[index]
+
+                # If the car can move (no impact with the car in front), update the car position to the new position
+                if car.ability_to_move:
+                    car.position = after_moving
+                # Else, keep the car at the same position, effectively not making it move this time step
+                else:
+                    car.speed =  car.speed /2
+                    if self.no_impact(car, cars[index-1]):
+                        car.position = after_moving
+                        car.speed = car.speed *2
+                    else:
+                        car.position = before_moving
+
+            # Append the positions of all the cars after one time step
+            position_history.append(cars.get_positions().copy())
+            time += self.time_step
+
+        return time, position_history
---
+++ main_example.py
@@ -0,0 +1,33 @@
+import numpy as np
+import car_class as c
+import cars_class as cs
+import road_class as r
+import dynamics_class as d
+import plotting_simulation as p
+import matplotlib
+
+# For the animated plot
+matplotlib.use('TkAgg') 
+
+# Set a time step for the whole simulation
+time_step = 0.5
+
+# Create at most 8 cars with different positions and speeds, assuring that the cars are on the road and not overlapping (otherwise the program will stop)
+# Ensure that car 1 is the one at the front of the road, car 2 is the one behind car 1, etc
+car = []
+i = 0
+
+for position in range(100,0,-8):
+    car.append(c.Car(np.array([position - 3, position]), 10))
+    i += 1
+
+# Create a list of cars
+cars = cs.Cars([car_i for car_i in car])
+
+# Create a road with a starting position and an end position
+road = r.Road(0, 160, vertical_position = 160)
+
+initialize_dynamics = d.Dynamics(time_step)
+
+animation, time_to_exit = p.animated_plot(cars, road, time_step, initialize_dynamics)
+print(f'\nTime to clear the road: {time_to_exit} s')
